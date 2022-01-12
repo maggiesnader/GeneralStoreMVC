dcced403cb30097d09792de574f3cc78985272bf
Maggie <88403063+maggiesnader@users.noreply.github.com>
2022-01-12 15:15:44 -0500

    Edit and Details Views

--- GeneralStoreMVC/Controllers/ProductController.cs
+++ GeneralStoreMVC/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using GeneralStoreMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -53,7 +54,7 @@ namespace GeneralStoreMVC.Controllers
             return View(product);
         }
 
-        //POST: Product/Delete/{id}
+        //POST: DELETE  Product/Delete/{id}
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
@@ -63,5 +64,52 @@ namespace GeneralStoreMVC.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //GET: EDIT  Product/Edit/{id}
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        //POST: EDIT  Product/Edit/{id}
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(product).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //GET: DETAILS Product/Details/{id}
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product product = _db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
     }
 }
