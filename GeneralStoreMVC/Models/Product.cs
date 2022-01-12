ecaf6d9bdb5a7969b14d173caa42b659e14662bc
Maggie <88403063+maggiesnader@users.noreply.github.com>
2022-01-11 20:45:50 -0500

    Add project files.

---
+++ GeneralStoreMVC/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreMVC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "ProductName")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "# In Stock")]
+        public int InventoryCount { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        [Display(Name = "It is Food")]
+        public bool IsFood { get; set; }
+    }
+}
